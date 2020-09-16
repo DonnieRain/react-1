9c2d0e6c64ae29c7f778e4d94a638da8cdc6c75b
DonnieRain <donnierevenge@gmail.com>
2020-09-16 11:25:04 +0300

    home-1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import Content from './components/Content'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <div className="wrapper">
+                    <Sidebar />
+                    <Content />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <h1 className="content">Content</h1>
+        )
+    }
+}
+
+export default Content;
